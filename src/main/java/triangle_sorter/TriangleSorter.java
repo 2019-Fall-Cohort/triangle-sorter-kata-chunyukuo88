3c97396c90908bb3d499ec1783852050b7dc1f36
unknown <alexgochenour@gmail.com>
2019-09-07 23:08:26 -0400

    My first attempt. Everything worked until I added methods to check if the sides are able to form a triangle. The logic there caused the test to fail, as well as two earlier tests to fail. I don't really understand how or why this is happening and will come back to it tomorrow.

--- src/main/java/triangle_sorter/TriangleSorter.java
+++ src/main/java/triangle_sorter/TriangleSorter.java
@@ -1,7 +1,36 @@
 package triangle_sorter;
 
 public class TriangleSorter {
-    public String analyze(int sideA, int sideB, int sideC) {
-        return "Equilateral";
+    
+	//This is the "First Method"
+	public String analyze(double sideA, double sideB, double sideC) {
+		if (triangleChecker(sideA, sideB, sideC) == true)
+			return "That is not a triangle.";
+		else if (sideA == sideB && sideB == sideC) 
+	        return "Equilateral";	
+		else if (sideA == sideB || sideA == sideC || sideB == sideC)
+			return "Isoceles";
+		else if (pythagoreanTester(sideA, sideB, sideC) == true)
+			return "Right triangle";
+		else if (sideA != sideB && sideB != sideC)
+			return "Scalene";
+		else
+			return "";
     }
+	
+	public boolean pythagoreanTester(double a, double b, double c) 	{
+		double sumOfTwoSquares = (Math.pow(a, 2) + Math.pow(b, 2));
+		
+		if (Math.pow(sumOfTwoSquares, 0.5) == c)
+			return true; //this triangle is a right triangle.
+		else
+			return false; //this triangle is NOT a right triangle.
+	}
+	
+	public boolean triangleChecker (double a, double b, double c)	{
+		if (a + b > c)
+			return true; //i.e. the values DO form a triangle.
+		else
+			return false;//no triangle!
+	}
 }
--- src/test/java/triangle_sorter/TriangleSorterTest.java
+++ src/test/java/triangle_sorter/TriangleSorterTest.java
@@ -13,5 +13,51 @@ public class TriangleSorterTest {
         String triangleType = underTest.analyze(3, 3, 3);
         assertThat(triangleType, is("Equilateral"));
     }
+    
+    @Test
+    public void onlyTwoEqualSidesIsAnIsoceles() {
+        TriangleSorter underTest = new TriangleSorter();
+        String triangleType = underTest.analyze(1, 1, 3);
+        assertThat(triangleType, is("Isoceles"));
+    }
+    
+    @Test
+    public void  isScaleneTriangle() {
+    	TriangleSorter underTest = new TriangleSorter();
+    	String triangleType = underTest.analyze(1, 2, 3);
+    	assertThat(triangleType, is("Scalene"));
+    }
+    
+    @Test 
+//    Note that this is testing a different method from the previous three.
+    public void isRightTriangle() {
+    	TriangleSorter underTest = new TriangleSorter();
+    	boolean triangleType = underTest.pythagoreanTester(5,  12, 13);
+    	assertThat(triangleType, is(true));
+    }
+    
+    @Test
+//    Back to testing the analyze() method:
+    public void firstMethodNowIncludesPythagorean() {
+    	TriangleSorter underTest = new TriangleSorter();
+    	String triangleType = underTest.analyze(5, 12, 13);
+    	assertThat(triangleType, is("Right triangle"));
+    }
+    
+    @Test
+//  This also uses a separate method.
+    public void testIfSidesFormTriangle() {
+    	TriangleSorter underTest = new TriangleSorter();
+    	boolean triangleType = underTest.triangleChecker(1, 1, 10);
+    	assertThat(triangleType, is (false));
+    }
+    
+    @Test
+    //The big change here was changing the 
+    public void firstMethodNowIncludesTriangleChecker() {
+    	TriangleSorter underTest = new TriangleSorter();
+    	String triangleType = underTest.analyze(1, 1, 10);
+    	assertThat(triangleType, is("That is not a triangle"));
+    }
 }
 
